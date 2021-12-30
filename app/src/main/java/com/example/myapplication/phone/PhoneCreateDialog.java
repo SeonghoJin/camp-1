9c468e29b76e18ffb60f35d6818c3088cdd7c057
moretall <seongho4996@naver.com>
2021-12-30 11:16:56 +0900

    feat : toggle complete button, default image

--- app/src/main/java/com/example/myapplication/phone/PhoneCreateDialog.java
+++ app/src/main/java/com/example/myapplication/phone/PhoneCreateDialog.java
@@ -1,31 +1,27 @@
 package com.example.myapplication.phone;
 
-import android.app.Dialog;
 import android.content.Context;
+import android.content.res.Resources;
+import android.opengl.Visibility;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.example.myapplication.MainActivity;
+import androidx.annotation.RequiresApi;
+
 import com.example.myapplication.R;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
-public class PhoneCreateDialog extends BottomSheetDialog {
+import java.util.Arrays;
 
-    private Context context;
+public class PhoneCreateDialog extends BottomSheetDialog {
 
     Button cancelButton;
     Button completeButton;
@@ -34,16 +30,17 @@ public class PhoneCreateDialog extends BottomSheetDialog {
     EditText firstNameEditText;
     EditText lastNameEditText;
     EditText phoneNumberEditText;
-    Boolean existsFirstName;
-    Boolean existsLastName;
-    Boolean existsPhoneNumber;
+    Boolean existsFirstName = false;
+    Boolean existsLastName = false;
+    Boolean existsPhoneNumber = false;
     PhoneCreateCallback phoneCreateCallback;
+    Name name;
 
     public PhoneCreateDialog(Context context) {
        super(context);
-       this.context = context;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +49,8 @@ public class PhoneCreateDialog extends BottomSheetDialog {
         bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
         initDialog();
         addEventListeners(phoneCreateCallback);
+        onInputTextChanged(new Boolean[]{existsPhoneNumber, existsLastName, existsPhoneNumber});
+        onNameChanged(name);
     }
 
     public void setOnComplete(PhoneCreateCallback phoneCreateCallback){
@@ -60,24 +59,27 @@ public class PhoneCreateDialog extends BottomSheetDialog {
 
     private void initDialog(){
         completeButton = findViewById(R.id.complete_action);
-        System.out.println(completeButton);
         cancelButton = findViewById(R.id.cancel_action);
         defaultProfileImage = findViewById(R.id.default_profile_image);
         defaultTextView = findViewById(R.id.default_profile_word);
         firstNameEditText = findViewById(R.id.input_first_name);
         lastNameEditText = findViewById(R.id.input_last_name);
         phoneNumberEditText = findViewById(R.id.input_phone_number);
+        name = new Name(firstNameEditText, lastNameEditText);
     }
 
     public BottomSheetDialog addEventListeners(PhoneCreateCallback callback){
         this.completeButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View view) {
-                String firstName = firstNameEditText.getText().toString();
-                String lastName = lastNameEditText.getText().toString();
-                String phoneNumber = phoneNumberEditText.getText().toString();
-                callback.start(phoneNumber, lastName, firstName);
-                dismiss();
+                if(canClickedCompleteButton(new Boolean[]{existsFirstName, existsLastName, existsPhoneNumber})){
+                    String firstName = firstNameEditText.getText().toString();
+                    String lastName = lastNameEditText.getText().toString();
+                    String phoneNumber = phoneNumberEditText.getText().toString();
+                    callback.start(phoneNumber, lastName, firstName);
+                    dismiss();
+                }
             }
         });
 
@@ -99,9 +101,12 @@ public class PhoneCreateDialog extends BottomSheetDialog {
 
             }
 
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void afterTextChanged(Editable editable) {
-                System.out.println(editable.toString());
+                existsFirstName = editable.toString().length() > 0;
+                onInputTextChanged(new Boolean[]{existsPhoneNumber, existsLastName, existsFirstName});
+                onNameChanged(name);
             }
         });
 
@@ -116,9 +121,12 @@ public class PhoneCreateDialog extends BottomSheetDialog {
 
             }
 
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void afterTextChanged(Editable editable) {
-
+                existsLastName = editable.toString().length() > 0;
+                onInputTextChanged(new Boolean[]{existsPhoneNumber, existsLastName, existsFirstName});
+                onNameChanged(name);
             }
         });
 
@@ -133,13 +141,88 @@ public class PhoneCreateDialog extends BottomSheetDialog {
 
             }
 
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void afterTextChanged(Editable editable) {
-
+                existsPhoneNumber = editable.toString().length() > 0;
+                onInputTextChanged(new Boolean[]{existsPhoneNumber, existsLastName, existsFirstName});
             }
         });
 
         return this;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void onInputTextChanged(Boolean[] booleans){
+        if(canClickedCompleteButton(booleans)){
+            enableCompleteButton();
+        } else {
+            unableCompleteButton();
+        }
+    }
+
+    private void onNameChanged(Name name) {
+        try {
+            String ellipseName = name.getEllipseName();
+            int currentEllipseNameLength = ellipseName.length();
+
+            if(currentEllipseNameLength == 0){
+                setVisibleDefaultProfileImage();
+                setUnVisibleDefaultProfileTextView();
+                return;
+            }
+
+            if(currentEllipseNameLength == 1) {
+                setUnVisibleDefaultProfileImage();
+                setVisibleDefaultProfileTextView();
+                defaultTextView.setText(ellipseName);
+                return;
+            }
+
+            if(currentEllipseNameLength == 2){
+                defaultTextView.setText(ellipseName);
+                return;
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private Boolean canClickedCompleteButton(Boolean[] booleans){
+        return Arrays.stream(booleans).reduce(((total, currentBoolean) -> {
+            return total | currentBoolean;
+        })).get();
+    }
+
+    private void enableCompleteButton(){
+        completeButton.setTextColor(getResource().getColor(R.color.iphone_links));
+    }
+
+
+    private void unableCompleteButton(){
+        completeButton.setTextColor(getResource().getColor(R.color.iphone_settings));
+    }
+
+    private void setVisibleDefaultProfileImage(){
+        this.defaultProfileImage.setVisibility(View.VISIBLE);
+    }
+
+    private void setUnVisibleDefaultProfileImage(){
+        this.defaultProfileImage.setVisibility(View.INVISIBLE);
+    }
+
+    private void setVisibleDefaultProfileTextView(){
+        this.defaultTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void setUnVisibleDefaultProfileTextView(){
+        this.defaultTextView.setVisibility(View.INVISIBLE);
+    }
+
+
+    protected Resources getResource(){
+        return getContext().getResources();
+    }
 }

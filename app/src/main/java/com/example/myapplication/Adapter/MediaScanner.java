11036a509f6cd2026be32dc2d16adc6c6648eaed
seohyeon0322 <asdf0322@kaist.ac.kr>
2021-12-31 20:35:57 +0900

    delete photo & apply photoView

--- app/src/main/java/com/example/myapplication/Adapter/MediaScanner.java
+++ app/src/main/java/com/example/myapplication/Adapter/MediaScanner.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.myapplication.Adapter;
 
 import android.content.Context;
 import android.media.MediaScannerConnection;
--- app/src/main/java/com/example/myapplication/GalleryImageAdapter.java
+++ app/src/main/java/com/example/myapplication/GalleryImageAdapter.java
@@ -43,22 +43,22 @@ public class GalleryImageAdapter extends BaseAdapter {
     @Override
     //creating new imageView
     public View getView(int i, View convertView, ViewGroup viewGroup) {
-        ImageView imageView;
+        ImageView imageView = (ImageView)convertView;
 
         int rowWidth = (mMetrics.widthPixels - mMetrics.widthPixels/15) / 3;
 
-        if(null != convertView){
-            imageView = (ImageView)convertView;
-        }else{
+//        if(null != convertView){
+//            imageView = (ImageView)convertView;
+//        }else{
             Bitmap bmp = BitmapFactory.decodeFile(imageIDs.get(i)); //to save memory
             bmp = Bitmap.createScaledBitmap(bmp, 400,400,false);
             imageView = new ImageView(context);
             imageView.setLayoutParams(new GridView.LayoutParams(rowWidth, rowWidth));
-            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+             imageView.setScaleType(ImageView.ScaleType.FIT_XY);
             imageView.setPadding(1,1,1,1);
             imageView.setImageBitmap(bmp);
 
-        }
+//        }
 
         return imageView;
     }

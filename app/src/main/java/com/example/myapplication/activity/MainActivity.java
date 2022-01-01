1e8d85f820a5838ef3c97f8b070f1ba0a15e2d00
moretall <seongho4996@naver.com>
2022-01-01 22:51:52 +0900

    feat : expanded text 구현

--- app/src/main/java/com/example/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/example/myapplication/activity/MainActivity.java
@@ -8,15 +8,21 @@ import com.example.myapplication.fragment.BlurFragment;
 
 public class MainActivity extends BaseTabActivity {
 
+    BlurFragment blurFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
     public void inflateBlurView(Bitmap bitmap){
+        blurFragment = new BlurFragment(mainLayout, bitmap);
         getSupportFragmentManager().beginTransaction()
-                .add(R.id.main_frame_layout, new BlurFragment(mainLayout, bitmap)).addToBackStack(null).commit();
+                .add(R.id.main_frame_layout, blurFragment).addToBackStack(null).commit();
+    }
 
+    public void removeBlurView(){
+        getSupportFragmentManager().beginTransaction().remove(blurFragment).commit();
     }
 
 }
--- app/src/main/java/com/example/myapplication/fragment/BlurFragment.java
+++ app/src/main/java/com/example/myapplication/fragment/BlurFragment.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.fragment;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,6 +13,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.example.myapplication.R;
+import com.example.myapplication.activity.MainActivity;
+import com.skydoves.expandablelayout.ExpandableLayout;
 
 import jp.wasabeef.blurry.Blurry;
 
@@ -21,6 +24,8 @@ public class BlurFragment extends Fragment {
     private ViewGroup root;
     private Bitmap bitmap;
     private Button button;
+    private ExpandableLayout expandableLayout1;
+    private ExpandableLayout expandableLayout2;
 
 
     public BlurFragment(ViewGroup view, Bitmap bitmap) {
@@ -38,13 +43,24 @@ public class BlurFragment extends Fragment {
 
     private void initView(ViewGroup rootView){
         blurView = rootView.findViewById(R.id.blur_view);
+        expandableLayout1 = rootView.findViewById(R.id.expanded_menu);
+        expandableLayout2 = rootView.findViewById(R.id.expanded_menu1);
+        button = rootView.findViewById(R.id.pass_button);
+        expandableLayout1.parentLayout.setOnClickListener(view -> expandableLayout1.toggleLayout());
+        expandableLayout2.parentLayout.setOnClickListener(view -> expandableLayout2.toggleLayout());
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity)getActivity()).removeBlurView();
+            }
+        });
         initBlur();
     }
 
     private void initBlur(){
         blurView.setImageBitmap(bitmap);
         Blurry.with(getContext())
-                .sampling(10)
+                .sampling(4)
                 .from(bitmap)
                 .into(blurView);
     }

0f8378c4bd2f4265ed5c0c65f45cb3f0016cedff
seohyeon0322 <asdf0322@kaist.ac.kr>
2022-01-01 18:23:22 +0900

    gallery database

---
+++ app/src/main/java/com/example/myapplication/gallery/GalleryFolder.java
@@ -0,0 +1,23 @@
+package com.example.myapplication.gallery;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+
+import java.util.ArrayList;
+
+
+@Entity
+public class GalleryFolder {
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @ColumnInfo(name = "folder_name")
+    public String folderName;
+
+    @ColumnInfo(name = "images")
+    public ArrayList<String> images;
+}
